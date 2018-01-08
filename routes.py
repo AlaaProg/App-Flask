62b5293817c416b039a00d4f41d5f168ad029208
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-01-08 14:26:20 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,7 +1,15 @@
+# From File /app/
 from app import route,pages,app
 
+# route. ( GET POST PUT DELETE )
+## 
+route.GET ( "/"     , pages.home     )
+route.POST( "/"     , pages.home     )
+## 
+route.GET ( "/login" , pages.login    )
+route.POST( "/login" , pages.login    )
 
-route.GET("/",pages.index)
 
 
-app.run(port=8010,debug=True)
+# Run App 
+app.run(port=8010)
