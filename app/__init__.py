9b8ea23d4af538b7b83a76df500f7ac54341186c
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-01-13 09:54:05 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,38 @@
 # -*- coding: utf-8 -*-
  
 import os
-###
+##
 from flask import Flask
 from app.config    import config
 ###
 from app.model     import Router
 from app.model     import SendMail
 from app.model     import Token
-###
+from app.model     import Cookies
+from app.model     import DBJson
+#
 from app.Databases import SQLite
-###
+#
 from app.Controller import Pages
 
 app = Flask(os.getcwd())
+
 config(app)
 
 class model:
+	
+
 	def __init__(self):
 
-		self.mail   = SendMail (  app)
-		self.token  = Token    (  app)
-		self.sqlite = SQLite   (  app)
+		self.mail    = SendMail (  app)
+		self.token   = Token    (  app)
+		self.sqlite  = SQLite   (  app)
+		self.dbJson  = DBJson   (  app)
+		self.cookies = Cookies
 
+
+	def __dir__(self): 
+
+		return ['mail', 'sqlite', 'token']
 route  = Router(app)
 pages  = Pages(model())
