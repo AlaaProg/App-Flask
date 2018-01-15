68c7b69a11baf71c68e7f2335b4612eb2c53d83c
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-01-15 06:49:44 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -19,24 +19,28 @@ class config():
             # Config Mail 
             'MAIL_SERVER'   : 'smtp.gmail.com',
             'MAIL_PORT'     : 465,
-            'MAIL_USERNAME' : '',
-            'MAIL_PASSWORD' : '',
+            'MAIL_USERNAME' : 'mail@gmail.com',
+            'MAIL_PASSWORD' : 'password',
             'MAIL_USE_TLS'  : False,
             'MAIL_USE_SSL'  : True,
 
 
             #JSON
-            "token_file"         : "app/Json/token.json",
-            "DB:Json"            : "app/Json/data.json",
+            "token_file"         : "app/DB/Json/token.json",
+            "DB:Json"            : "app/DB/Json/data.json",
 
             #DB
-            "DB:SQLite"     : "app/Databases/dbite/db.sql",
+            "DB:SQLite"     : "app/DB/dbite/db.sql",
+            "DB:MySQL"      : { 
+
+                                "user"      :"root",
+                                "host"      :"localhost",
+                                "database"  :"test",
+                                "password"  :"" 
+
+                            }
 
         }
 
 
-
         app.config.update(config)
-        
-
-
