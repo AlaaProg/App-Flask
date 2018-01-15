6b583e9efa49ae5a347cf41e08ab64ffa4555f64
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-01-15 06:48:51 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,12 @@
-# -*- coding: utf-8 -*-
- 
-import os
-##
+import importlib as lib ,os
+#####
 from flask import Flask
 from app.config    import config
-###
-from app.model     import Router
-from app.model     import SendMail
-from app.model     import Token
-from app.model     import Cookies
-from app.model     import DBJson
-#
-from app.Databases import SQLite
-#
-from app.Controller import Pages
+#####
+from app.model     import Router,SendMail,Token,Cookies
+#####
+from app.DB import SQLite,MySQL,DBJson
+
 
 app = Flask(os.getcwd())
 
@@ -27,12 +20,31 @@ class model:
 		self.mail    = SendMail (  app)
 		self.token   = Token    (  app)
 		self.sqlite  = SQLite   (  app)
+		#self.mysql   = MySQL    (  app)
 		self.dbJson  = DBJson   (  app)
 		self.cookies = Cookies
 
 
 	def __dir__(self): 
 
-		return ['mail', 'sqlite', 'token']
+		return ['mail', 'sqlite', 'token',"mysql","cookies","dbJson"]
+
+class Controllers():
+
+	def __init__(self,App):
+		self.Controll = {}
+		for  i in os.listdir("app/Controller"):
+			if not i.startswith("__"):
+				model = i.strip(".py")
+				Cl = lib.import_module("app.Controller.%s"%model,model)
+				self.Controll.update({model:getattr(Cl,model)(App)})
+		
+	def __getattr__(self,k):
+		return self.Controll.get(k)
+
+	def __dir__(self):
+		return self.Controll.keys()
+
+
 route  = Router(app)
-pages  = Pages(model())
+view  = Controllers(model())
